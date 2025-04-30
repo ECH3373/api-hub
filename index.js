6812f151376c3bbd1d573d11c4eff8d327d16047
37r4n <e.37r4n@gmail.com>
2025-04-30 13:27:28 -0600

    create public directory

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express, { json } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
@@ -7,6 +8,7 @@ import { hub } from './src/hub/index.js';
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(express.static(path.join(process.cwd(), 'public')));
 app.use('/storage', express.static('storage'));
 
 app.use('/api/v1/hub', hub.router);
